6afdeecd3042cdbbb10e5d9833d231b11f9ab69f
Pushpendra paliwal <ppaliwal304@gmail.com>
2024-07-29 16:44:45 +0530

    commit to package 13072024

---
+++ src/ex_13072024/Lab013.java
@@ -0,0 +1,17 @@
+package ex_13072024;
+
+public class Lab013 {
+    public static void main(String[] args) {
+        int age =9;
+        //System.out.println("%d",age);
+        System.out.printf("%d",age);// output given according to data type format
+        //%d ->byte, short, int, long
+        //%s -> string
+        //%c -> char
+        // %f-> float
+        //concatenate + with int and string, int int ,string string, somemore
+        // modulus opeartor
+        // constant
+        //
+    }
+}
